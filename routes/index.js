d40051a21d5d80fa32e60575073865624ff8580b
Robert Bunch <rob@digitalcrafts.com>
2017-02-02 16:45:03 -0500

    params and query, set up routes and single movie page

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var config = require('../config/config');
 
-var config = {
-    baseUrl: 'http://api.themoviedb.org/3/',
-	imageBase: 'http://image.tmdb.org/t/p/w300',
-	imageBaseFull: 'http://image.tmdb.org/t/p/original',
-    nowPlayingEP: 'movie/now_playing?',
-    api_key: '&api_key=fec8b5ab27b292a68294261bb21b04a5',
-    bpMovies11: 'discover/movie?with_people=287&primary_release_year=2011&sort_by=vote_average.desc',
-};
+config.sayHello();
+
+// req.body
+// req.params
+// req.query
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -30,6 +28,31 @@ router.get('/search', function(req, res, next){
 	res.render('search', {});
 });
 
+router.get('/movie/:movieId', (req,res,next)=>{
+	var url = config.baseUrl+ 'movie/'+req.params.movieId+'?api_key=fec8b5ab27b292a68294261bb21b04a5';
+	request.get(url, (error, response, movieData)=>{
+		movieData = JSON.parse(movieData);
+		res.render('singleMovie', {movieData: movieData});
+	});
+
+	// res.send("You searched for " + req.params.movieId);
+});
+
+router.get('/search/:searchString', (req, res, next) =>{
+	var searchString = req.params.searchString;
+	var queryUrl = config.baseUrl + 'search/movie?'+config.api_key+'&query='+searchString;
+	// res.send(queryUrl);
+	request.get(queryUrl, (error, response, searchData)=>{
+		searchData = JSON.parse(searchData);
+		res.render('index',{
+			movieData: searchData,
+			imageUrl: config.imageBase,
+			searchString: searchString
+		})
+	})
+
+});
+
 router.get('/searchMovie', function(req, res, next){
 	res.send("Haha, I'm a get route.");
 })
@@ -42,7 +65,8 @@ router.post('/searchMovie', function(req, res, next){
 		searchData = JSON.parse(searchData);
 		res.render('index',{
 			movieData: searchData,
-			imageUrl: config.imageBase
+			imageUrl: config.imageBase,
+			searchString: searchString
 		})
 	})
 });
